8edbaa5954e79349d9b91f758f933d5f936e1e27
Dave Feucht <poetas@gmail.com>
2018-03-12 22:07:01 +0100

    Added babel processing to convert ES6 in src/* to standard js in js/*

--- js/site.js
+++ js/site.js
@@ -3,33 +3,34 @@
 var access_token = "";
 
 function foursquareAuth(step, ...data) {
-    if(step === 'redirect') {
+    if (step === 'redirect') {
         location.replace("https://foursquare.com/oauth2/authenticate?client_id=ZK1LL4LNA35TMQHPJORGHTWMP1LJVWLTVPHA4FCVBMLLZHJ3&response_type=code&redirect_uri=http://dev.throughapinhole.com/foursquare/");
-    }
-    else if (step === 'get-token') {
-        
+    } else if (step === 'get-token') {
+        alert(data[0]);
     }
 }
 
 function getLocation() {
-  if("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-    function(position) {
-        $('.message-div').text('Your position is: ' + position.coords.latitude + ", " + position.coords.longitude);
-        alert(position.coords.latitude + ", " + position.coords.longitude);
-    },
-    function(error) {
-        alert(error.code + ": " + error.message); 
-    });
-  }
-  else {
-      $('.message-div').text('Your browser does not support location services.');
-  }
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            $('.message-div').text('Your position is: ' + position.coords.latitude + ", " + position.coords.longitude);
+        }, function (error) {
+            $('.message-div').text(error.code + ": " + error.message);
+        });
+    } else {
+        $('.message-div').text('Your browser does not support location services.');
+    }
 }
 
-$(document).ready(function() {
-    var urlParams = new URLSearchParams(window.location.search);
-    if(urlParams.has('code')) {
-    foursquareAuth('get-token');
-    getLocation();
+$(document).ready(function () {
+    let urlParams = new URLSearchParams(window.location.search);
+    let code = "";
+
+    if (urlParams.has('code')) {
+        code = urlParams.get('code');
+        foursquareAuth('get-token', code);
+    } else {
+        getLocation();
+        foursquareAuth('redirect');
+    }
 });
