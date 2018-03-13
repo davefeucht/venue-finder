3cc7bd2e17848546398db5c672291ba579894f70
Dave Feucht <poetas@gmail.com>
2018-03-13 13:52:20 +0100

    Added inputs for radius and type of venue, and a search button. Search button now runs the search using the input values and displays name and address of results below the input boxes.

--- js/site.js
+++ js/site.js
@@ -1,11 +1,9 @@
 "use strict";
 
+const REQUEST_BASE_URL = "https://api.foursquare.com/v2/venues/search?ll=";
 const CLIENT_ID = "ZK1LL4LNA35TMQHPJORGHTWMP1LJVWLTVPHA4FCVBMLLZHJ3";
 const CLIENT_SECRET = "1KG5RCUPH0RDODKDE4KOIOAFL13KPV41AZNJE4ZN4WY1VDVP";
-const REDIRECT_URL = "https://dev.throughapinhole.com/foursquare";
-const REQUEST_BASE_URL = "https://api.foursquare.com/v2/venues/search?ll=";
-
-let access_token = "";
+const INTENT = "checkin";
 
 function createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
@@ -21,39 +19,26 @@ function createCORSRequest(method, url) {
     return xhr;
 }
 
-function foursquareAuth(step, ...data) {
-    if (step === "redirect") {
-        location.replace("https://foursquare.com/oauth2/authenticate?client_id=ZK1LL4LNA35TMQHPJORGHTWMP1LJVWLTVPHA4FCVBMLLZHJ3&response_type=code&redirect_uri=" + REDIRECT_URL);
-    } else if (step === "get-token") {
-        let url = "https://foursquare.com/oauth2/access_token?client_id=ZK1LL4LNA35TMQHPJORGHTWMP1LJVWLTVPHA4FCVBMLLZHJ3&client_secret=1KG5RCUPH0RDODKDE4KOIOAFL13KPV41AZNJE4ZN4WY1VDVP&grant_type=authorization_code&redirect_uri=" + REDIRECT_URL + "&code=" + data[0];
-        let xhr = createCORSRequest("GET", url);
-        if (!xhr) {
-            $(".message-div").text("CORS is not supported by your browser");
-        } else {
-            xhr.onload = function () {
-                let responseText = xhr.responseText;
-                access_token = responseText;
-                alert(access_token);
-            };
-            xhr.onerror = function () {
-                $(".message-div").text("There was an error sending the request.");
-            };
-
-            xhr.send();
-        }
-    }
-}
-
 function foursquareRequest(radius, type, ...coordinates) {
+    let radius_meters = radius * 1000;
     let now = new Date();
-    let date_string = now.getFullYear().toString() + (now.getMonth() < 10 ? "0" + (now.getMonth() + 1).toString() : (now.getMonth() + 1).toString()) + now.getDate().toString();
-    let url = REQUEST_BASE_URL + coordinates[0] + "," + coordinates[1] + "&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=" + date_string;
+    let date_string = now.getFullYear().toString() + (now.getMonth() < 10 ? "0" + (now.getMonth() + 1).toString() : (now.getMonth() + 1).toString()) + (now.getDate < 10 ? "0" + now.getDate().toString() : now.getDate().toString());
+    let url = REQUEST_BASE_URL + coordinates[0] + "," + coordinates[1] + "&intent=" + INTENT + "&radius=" + radius_meters + "&query=" + type + "&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=" + date_string;
     let xhr = createCORSRequest("GET", url);
+
     if (!xhr) {
         $(".message-div").text("CORS is not supported by your browser");
     } else {
         xhr.onload = function () {
-            $(".message-div").text(xhr.responseText);
+            let responseObject = JSON.parse(xhr.responseText);
+            let venues = responseObject.response.venues;
+
+            $(".results-div").text("");
+            venues.forEach(venue => {
+                $(".results-div").append("<h3>" + venue.name + "</h3>");
+                $(".results-div").append(venue.location.address !== undefined ? venue.location.address + "<br />" : " ");
+                $(".results-div").append((venue.location.postalCode !== undefined ? venue.location.postalCode : " ") + " " + (venue.location.city !== undefined ? venue.location.city : " ") + "<br />");
+            });
         };
         xhr.onerror = function () {
             $(".message-div").text("There was an error sending the request.");
@@ -63,11 +48,13 @@ function foursquareRequest(radius, type, ...coordinates) {
     }
 }
 
-function getLocation() {
+function search() {
+    let radius = $("#radius").val();
+    let search_type = $("#venue-type").val();
 
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function (position) {
-            foursquareRequest(3, "restaurant", position.coords.latitude, position.coords.longitude);
+            foursquareRequest(radius, search_type, position.coords.latitude, position.coords.longitude);
             $(".message-div").text("Your position is: " + position.coords.latitude + ", " + position.coords.longitude);
         }, function (error) {
             $(".message-div").text(error.code + ": " + error.message);
@@ -79,14 +66,5 @@ function getLocation() {
 }
 
 $(document).ready(function () {
-    let urlParams = new URLSearchParams(window.location.search);
-    let code = "";
-
-    if (urlParams.has("code")) {
-        code = urlParams.get("code");
-        foursquareAuth("get-token", code);
-    } else {
-        getLocation();
-        //foursquareAuth("redirect");
-    }
+    $("#search-button").on("click", search);
 });
